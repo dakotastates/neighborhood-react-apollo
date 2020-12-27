161764eb43ea90da32b92072279f9bae7d672f7e
Dakota States <dakota.states@gmail.com>
2020-12-26 22:53:31 -0800

    Apollo reconfiguration headers

--- src/index.js
+++ src/index.js
@@ -3,13 +3,61 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import { ApolloClient, InMemoryCache } from '@apollo/client';
+// import { gql } from '@apollo/client';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { ApolloProvider } from '@apollo/react-hooks';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: 'http://localhost:3000/graphql',
+  headers: {
+
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': true,
+    
+  },
+  fetch,
+  // fetchOptions: {
+  //   mode: 'no-cors',
+  // },
+  // uri: 'https://graphql-pokemon.now.sh/'
+})
+
+const client = new ApolloClient({
+  cache,
+  link
+})
+
+// const client = new ApolloClient({
+//   uri: 'https://48p1r2roz4.sse.codesandbox.io',
+//   cache: new InMemoryCache()
+// });
+//
+// client
+//   .query({
+//     query: gql`
+//       query GetRates {
+//         rates(currency: "USD") {
+//           currency
+//         }
+//       }
+//     `
+//   })
+//   .then(result => console.log(result));
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+ReactDOM.render(<ApolloProvider client={client}><App /></ApolloProvider>, document.getElementById('root'));
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
